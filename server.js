6cfb63a0f6143b0fccc832ec5cedcfd423d4e704
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-05-13 02:21:37 -0500

    added html routes

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")))
 
+//html routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'))
+});
+
+//api routes
 
 app.listen(PORT, () => console.log(`noteTaker app is listening on PORT ${PORT}`));
