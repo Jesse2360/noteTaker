b714177ed01f036223eca94b71cf6db8f9c4e07c
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-05-15 14:40:43 -0500

    updated file

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")))
 
+
 //html routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
-});
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'))
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
 //api routes
